a2625b2cfc0dbb5c174779758ee3c1cd86a275c7
aurakhov <aurakhovtel@gmail.com>
2023-10-19 13:25:02 +0500

    Course2LR3

--- src/main/java/ru/aurakhov/mysecondtestapspringbootlr5/model/Positions.java
+++ src/main/java/ru/aurakhov/mysecondtestapspringbootlr5/model/Positions.java
@@ -5,13 +5,23 @@ import lombok.Getter;
 @Getter
 public enum Positions {
 
-    DEV(2.2),
-    HR(1.2),
-    TL(2.6);
+
+    DEV(2.2, false),
+    HR(1.2, false),
+    TL(2.6, false),
+    IT(1.7, false),
+    PR(2.3, false),
+    CEO(3.0, true),
+    MGMT(2.7, true);
 
     private final double positionCoefficient;
+    private final boolean isManager;
 
-    Positions(double positionCoefficient) {
+    Positions(double positionCoefficient, boolean isManager) {
         this.positionCoefficient = positionCoefficient;
+        this.isManager = isManager;
     }
+
+
+
 }
